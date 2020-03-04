9f563db1f2b77bfcfd81f880a76bd5015e41d9e5
BuRNIngsuperman <2502975747@qq.com>
2020-03-04 21:54:04 +0800

    add home page

--- src/main/java/com/example/wenda/Controller/IndexController.java
+++ src/main/java/com/example/wenda/Controller/IndexController.java
@@ -1,4 +1,15 @@
 package com.example.wenda.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping(path = {"/","/index"},method = {RequestMethod.GET,RequestMethod.POST})
+    public String index(){
+        return "index";
+    }
 }
