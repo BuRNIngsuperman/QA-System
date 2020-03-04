09fa01fbe7723237db7bedf0ac9a0c982164f4c5
BuRNIngsuperman <2502975747@qq.com>
2020-03-05 07:53:11 +0800

    update index page

--- src/test/java/com/example/wenda/InitDataBasesTests.java
+++ src/test/java/com/example/wenda/InitDataBasesTests.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -27,18 +28,25 @@ public class InitDataBasesTests {
     @Test
     public void contextLoads() {
 //        Random random = new Random();
-//        for (int i = 0; i < 11; ++i) {
+        for (int i = 0; i < 11; ++i) {
 //            User user = new User();
 //            user.setHeadUrl(String.format("http://images.nowcoder.com/head/%dt.png", random.nextInt(1000)));
 //            user.setName(String.format("USER%d", i));
 //            user.setPassword("");
 //            user.setSalt("");
 //            userMapper.addUser(user);
-//        }
+            Question question = new Question();
+            question.setCommentCount(i);
+            Date date = new Date();
+            date.setTime(date.getTime() + 1000 * 3600 * 5 * i);
+            question.setCreatedDate(date);
+            question.setUserId(i + 1);
+            question.setTitle(String.format("TITLE{%d}", i));
+            question.setContent(String.format("Balaababalalalal Content %d", i));
+            questionMapper.addQuestion(question);
 
-        List<Question> questions = questionMapper.selectLatestQuestions(0, 1, 10);
-        for (Question q: questions){
-            System.out.println(q.getContent());
         }
+
+
     }
 }
